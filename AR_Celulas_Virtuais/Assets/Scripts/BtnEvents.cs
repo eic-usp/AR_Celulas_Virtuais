95c5de8b0693d5e9e7785986d62bf8ec7761e554
EricRC123 <eric.r.c.13031@gmail.com>
2023-04-28 14:15:17 -0300

    Adicionando alguns scripts do outro projeto

---
+++ AR_Celulas_Virtuais/Assets/Scripts/BtnEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnEvents : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    
+    public void OnBtnClick()
+    {
+        Camera.main.GetComponent<MouseOrbitSample>().RefreshCamera(target.transform.Find("_cameraDirection"), 1f, 2f);
+        GameManager.ActiveCell = target;
+
+    }
+
+}
