ad05e3d746647164928463f957002cdeccfe9819
Eric Rodrigues das Chagas <eric.r.c.13031@gmail.com>
2023-07-01 18:53:13 -0300

    Recriando  menu (de novo)

---
+++ AR_Celulas_Virtuais/Assets/Scripts/Menu_Script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+
+
+    public void goInitialScene() => SceneManager.LoadScene(1); 
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
