339522f35aec0d4d4113054690272bc7c174c77e
Eric Rodrigues das Chagas <eric.r.c.13031@gmail.com>
2023-07-03 18:19:57 -0300

    Tela inicial e tela de créditos
    Infos essenciais da tela inicial e da de créditos

--- AR_Celulas_Virtuais/Assets/Scripts/Menu_Script.cs
+++ AR_Celulas_Virtuais/Assets/Scripts/Menu_Script.cs
@@ -10,12 +10,27 @@ public class MenuScript : MonoBehaviour
 
     public void goInitialScene() => SceneManager.LoadScene(1); 
 
-
+    public GameObject CanvaMenu;
+    public GameObject CanvaCredits;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        CanvaMenu.SetActive(true);
+        CanvaCredits.SetActive(false);
+    
+    }
+
+    public void GoCredits(){
+        CanvaMenu.SetActive(false);
+        CanvaCredits.SetActive(true);
+    
+    }
+
+    public void BackCredits(){
+        CanvaMenu.SetActive(true);
+        CanvaCredits.SetActive(false);
         
     }
 
