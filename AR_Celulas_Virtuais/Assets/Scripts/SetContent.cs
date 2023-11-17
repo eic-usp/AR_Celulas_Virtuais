568f18200ad2cf7b568a9850449584fb7d835f2d
EricRC-01 <143744864+infinitymayh@users.noreply.github.com>
2023-11-17 16:47:13 -0300

    Criando data e scripts novos
    ajuda do Lucas

---
+++ AR_Celulas_Virtuais/Assets/Scripts/SetContent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SetContent : MonoBehaviour
+{
+    [SerializeField] private OrganelleData organelleData;
+    [SerializeField] private GameObject Box;
+    [SerializeField] private TextMeshProUGUI organelleNameText;
+    [SerializeField] private TextMeshProUGUI organelleDescriptionText;
+    
+
+    private void OnMouseDown() {
+        
+        Box.SetActive(true);
+        organelleNameText.text = organelleData.Name;
+        organelleDescriptionText.text = organelleData.Description;
+    }
+}
